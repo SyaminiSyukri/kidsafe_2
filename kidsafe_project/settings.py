8d6c3520238eb89ff2cee869f1c6dfb7a4560476
SyaminiSyukri <23FTT1101@student.pb.edu.bn>
2025-04-25 18:17:22 +0800

    updating

--- kidsafe_project/settings.py
+++ kidsafe_project/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'django-insecure-5i(rtogbnt!xj-5s$z^usdvwewn@3j^mfpwhd=buxdb3bdw-d+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['127.0.0.1','192.168.100.235','192.168.233.122']
 
 
 # Application definition
